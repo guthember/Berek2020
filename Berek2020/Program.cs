cc49fbdb1e42d9049fd5a1cf26087d5f73485091
guthember <guthember@gmail.com>
2021-09-17 09:20:29 +0200

    2. feladat megoldva

--- Berek2020/Program.cs
+++ Berek2020/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Berek2020
 {
@@ -12,8 +13,31 @@ namespace Berek2020
 
     static void Main(string[] args)
     {
+      MasodikFeladat();
 
 
+      Console.ReadKey();
+    }
+
+    private static void MasodikFeladat()
+    {
+      Console.WriteLine("2. feladat: adatok beolvasása");
+
+      StreamReader be = new StreamReader("berek2020.txt");
+
+      be.ReadLine();
+
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+
+        dolgozok.Add(new Dolgozo(
+            a[0], a[1], a[2], int.Parse(a[3]), int.Parse(a[4])
+          ));
+      }
+
+
+      be.Close();
     }
   }
 }
